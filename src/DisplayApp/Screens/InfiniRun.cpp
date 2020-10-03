438ad875df4f537024d5927bb304ec7b052ad173
Sravan <65145081+ZephyrLabs@users.noreply.github.com>
2020-10-03 22:09:34 +0530

    Update InfiniRun.cpp

--- src/DisplayApp/Screens/InfiniRun.cpp
+++ src/DisplayApp/Screens/InfiniRun.cpp
@@ -9,18 +9,29 @@ extern lv_font_t jetbrains_mono_bold_20;
 
 //extern static lv_img_dsc_t bitmap; 
 
-lv_obj_t *img_src;
+lv_obj_t *img_src_1;
+lv_obj_t *img_src_2;
 
 InfiniRun::InfiniRun(Pinetime::Applications::DisplayApp *app, Pinetime::Components::LittleVgl& lvgl) : Screen(app){
   app->SetTouchMode(DisplayApp::TouchModes::Polling);
-  bitmap.header.always_zero = 0;
-  bitmap.header.w = 240;
-  bitmap.header.h = 240;
-  bitmap.data_size = 240 * 240 * LV_COLOR_SIZE / 8;
-  bitmap.header.cf = LV_IMG_CF_TRUE_COLOR;
-  bitmap.data = bitmap_map;  
-  img_src = lv_img_create(lv_scr_act(), NULL);
-  lv_img_set_src(img_src, &bitmap);
+  
+  paddle.header.always_zero = 0;
+  paddle.header.w = 4;
+  paddle.header.h = 60;
+  paddle.data_size = 60 * 4 * LV_COLOR_SIZE / 8;
+  paddle.header.cf = LV_IMG_CF_TRUE_COLOR;
+  paddle.data = paddle_map;  
+  img_src_1 = lv_img_create(lv_scr_act(), NULL);
+  lv_img_set_src(img_src_1, &paddle);
+  
+  ball.header.always_zero = 0;
+  ball.header.w = 24;
+  ball.header.h = 24;
+  ball.data_size = 24 * 24 * LV_COLOR_SIZE / 8;
+  ball.header.cf = LV_IMG_CF_TRUE_COLOR;
+  ball.data = ball_map;  
+  img_src_2 = lv_img_create(lv_scr_act(), NULL);
+  lv_img_set_src(img_src_2, &ball);
 }
 
 InfiniRun::~InfiniRun() {
@@ -40,13 +51,13 @@ bool InfiniRun::OnButtonPushed() {
 
 bool InfiniRun::OnTouchEvent(Pinetime::Applications::TouchEvents event) { 
    
-  lv_obj_set_pos(img_src, 0, 0);  
+  lv_obj_set_pos(img_src_1, 0, 0);  
   return true; 
 }
 
 bool InfiniRun::OnTouchEvent(uint16_t x, uint16_t y) {
  
-  lv_obj_set_pos(img_src, x, y);
+  lv_obj_set_pos(img_src, 0, y - 30);
  // lvgl.SetFullRefresh(Components::LittleVgl::FullRefreshDirections::None);
  // lvgl.FlushDisplay(&area, b);
   return true;
