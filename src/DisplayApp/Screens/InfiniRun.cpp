c3102a80da9f6dc31409cbf34b57fd83ff195994
Sravan <65145081+ZephyrLabs@users.noreply.github.com>
2020-10-03 20:18:50 +0530

    Update InfiniRun.cpp

--- src/DisplayApp/Screens/InfiniRun.cpp
+++ src/DisplayApp/Screens/InfiniRun.cpp
@@ -7,6 +7,15 @@ using namespace Pinetime::Applications::Screens;
 extern lv_font_t jetbrains_mono_extrabold_compressed;
 extern lv_font_t jetbrains_mono_bold_20;
 
+bitmap.header.always_zero = 0;
+bitmap.header.w = 240;
+bitmap.header.h = 240;
+bitmap.data_size = 240 * 240 * LV_COLOR_SIZE / 8;
+bitmap.header.cf = LV_IMG_CF_TRUE_COLOR;
+bitmap.data = bitmap_map;
+lv_obj_t *img_src = lv_img_create(lv_scr_act(), NULL);  
+lv_img_set_src(img_src, &bitmap); 
+
 InfiniRun::InfiniRun(Pinetime::Applications::DisplayApp *app, Pinetime::Components::LittleVgl& lvgl) : Screen(app){
   app->SetTouchMode(DisplayApp::TouchModes::Polling);
 }
@@ -26,17 +35,14 @@ bool InfiniRun::OnButtonPushed() {
   return true;
 }
 
-bool InfiniRun::OnTouchEvent(Pinetime::Applications::TouchEvents event) {
-  
-  bitmap.header.always_zero = 0;
-  bitmap.header.w = 240;
-  bitmap.header.h = 240;
-  bitmap.data_size = 240 * 240 * LV_COLOR_SIZE / 8;
-  bitmap.header.cf = LV_IMG_CF_TRUE_COLOR;
-  bitmap.data = bitmap_map;
-  lv_obj_t *img_src = lv_img_create(lv_scr_act(), NULL);  
-  lv_img_set_src(img_src, &bitmap);  
-  lv_obj_set_pos(img_src, 0, 0);
-  
+bool InfiniRun::OnTouchEvent(Pinetime::Applications::TouchEvents event) { 
+  lv_obj_set_pos(img_src, 0, 0);  
   return true; 
+}
+
+bool InfiniPaint::OnTouchEvent(uint16_t x, uint16_t y) {
+  lv_obj_set_pos(img_src, x, y);
+  lvgl.SetFullRefresh(Components::LittleVgl::FullRefreshDirections::None);
+  lvgl.FlushDisplay(&area, b);
+  return true;
 }
