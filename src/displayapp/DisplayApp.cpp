3499d6b51c2a777e72374ee20d3f403e549ae64f
Sravan <65145081+ZephyrLabs@users.noreply.github.com>
2020-10-04 21:46:44 +0530

    Update DisplayApp.cpp

--- src/displayapp/DisplayApp.cpp
+++ src/displayapp/DisplayApp.cpp
@@ -199,8 +199,9 @@ void DisplayApp::RunningState() {
       case Apps::Meter: currentScreen.reset(new Screens::Meter(this)); break;
       case Apps::Gauge: currentScreen.reset(new Screens::Gauge(this)); break;
       case Apps::Paint: currentScreen.reset(new Screens::InfiniPaint(this, lvgl)); break;
-      case Apps::Brightness : currentScreen.reset(new Screens::Brightness(this, brightnessController)); break;
-      case Apps::Music : currentScreen.reset(new Screens::Music(this, systemTask.nimble().music())); break;
+      case Apps::Run: currentScreen.reset(new Screens::InfiniRun(this, lvgl)); break;
+      case Apps::Brightness: currentScreen.reset(new Screens::Brightness(this, brightnessController)); break;
+      case Apps::Music: currentScreen.reset(new Screens::Music(this, systemTask.nimble().music())); break;
       case Apps::FirmwareValidation: currentScreen.reset(new Screens::FirmwareValidation(this, validator)); break;
     }
     nextApp = Apps::None;
