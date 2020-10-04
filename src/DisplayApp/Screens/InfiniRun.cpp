b54ef098f9d009dd005e1efe14199a1c8c94a40a
Sravan <65145081+ZephyrLabs@users.noreply.github.com>
2020-10-04 15:26:40 +0530

    Update InfiniRun.cpp

--- src/DisplayApp/Screens/InfiniRun.cpp
+++ src/DisplayApp/Screens/InfiniRun.cpp
@@ -58,27 +58,26 @@ bool InfiniRun::Refresh() {
   
   if((counter++ % 5) == 0){
 
-  counter = 0;	
+    counter = 0;	
 	  
-  lv_obj_set_pos(img_src_2, pos_x + dx, pos_y + dy);
+    lv_obj_set_pos(img_src_2, pos_x + dx, pos_y + dy);
   
-  if(pos_y <= 0 || pos_y >= 228){
-    dy *= -1;
-  }
+    if(pos_y <= 0 || pos_y >= 228){
+      dy *= -1;
+    }
   
-  if(pos_x >= 228){
-    dx *= -1; 
-  }
+    if(pos_x >= 228){
+      dx *= -1; 
+    }
   
-  if(pos_x <= 20 && pos_y >= y_1 && pos_y <= y_2){
-    dx *= -1;          
-  }
+    if(pos_x <= 20 && pos_y >= y_1 && pos_y <= y_2){
+      dx *= -1;          
+    }
   
-  else if(pos_x <= -40){
-  pos_x = 120;
-  pos_y = 120; 
-  }
-  
+    else if(pos_x <= -40){
+      pos_x = 120;
+      pos_y = 120; 
+    } 
   }      
   return running;
 }
