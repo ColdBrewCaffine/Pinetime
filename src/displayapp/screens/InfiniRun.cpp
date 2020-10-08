c986666c227fb7a0ed58406df7f5be16470217d6
Sravan <65145081+ZephyrLabs@users.noreply.github.com>
2020-10-08 16:46:11 +0530

    Update InfiniRun.cpp

--- src/displayapp/screens/InfiniRun.cpp
+++ src/displayapp/screens/InfiniRun.cpp
@@ -17,8 +17,8 @@ int y_1 = 90;		// bottom extreme of the paddle
 int x_2 = 0;
 int y_2 = 150;		//top extreme of the paddle
 
-int pos_x = 108;	// Initial x_coordinate for the ball (12px offset from the center to counteract the ball's 24px size)
-int pos_y = 108;	// Initial y_coordinate for the ball
+int pos_x = 107;	// Initial x_coordinate for the ball (12px offset from the center to counteract the ball's 24px size)
+int pos_y = 107;	// Initial y_coordinate for the ball
 
 int dx = 3;		// Velocity of the ball in the x_coordinate
 int dy = 2;		// Velocity of the ball in the y_coordinate
@@ -34,7 +34,8 @@ InfiniRun::InfiniRun(Pinetime::Applications::DisplayApp *app, Pinetime::Componen
 	
   points = lv_label_create(lv_scr_act(), NULL);
   lv_label_set_text(points, "0");
-  lv_obj_align(points, lv_scr_act(), LV_ALIGN_IN_TOP_MID, 0, 0);
+  lv_obj_set_pos(points, 80, 0);	
+//lv_obj_align(points, lv_scr_act(), LV_ALIGN_IN_TOP_MID, 0, 0);
 	
   paddle.header.always_zero = 0;
   paddle.header.w = 4;
@@ -73,26 +74,28 @@ bool InfiniRun::Refresh() {
     lv_obj_set_pos(img_src_2, pos_x, pos_y);
   
     //checks if it has touched the sides (floor and ceiling)	  
-    if(pos_y <= 0 || pos_y >= 216){
+    if(pos_y <= 0 || pos_y >= 215){
       dy *= -1;
     }
   
     //checks if it has touched the side (left side)  
-    if(pos_x >= 216){
+    if(pos_x >= 215){
       dx *= -1; 
     }
   
     //checks if it is in the position of the paddle	  
-    if(pos_x >= 0 && pos_x < 4 && pos_y >= y_1 && pos_y <= y_2){
-      lv_obj_set_pos(img_src_2, 4, pos_y);
-      dx *= -1;   
-      score++;
-    }
+    if(pos_y >= y_1 && pos_y <= y_2){ 
+      if(pos_x >= 0 && pos_x < 4){    
+          lv_obj_set_pos(img_src_2, 5, pos_y);
+          dx *= -1;   
+          score++;
+        } 
+    }	
 	  
     //checks if it has gone behind the paddle
     else if(pos_x <= -40){
-      pos_x = 108;
-      pos_y = 108; 
+      pos_x = 107;
+      pos_y = 107; 
       score = 0;	    
     } 
     sprintf(Val, "%d", score);	
